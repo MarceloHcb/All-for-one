9631d60dada20aed82688f0de413c762798a0100
MarceloHcb <marcelohcb@msn.com>
2023-03-29 00:08:50 -0300

    fix

--- desafio15.sql
+++ desafio15.sql
@@ -1,2 +1,2 @@
 -- SELECT DATE_FORMAT(submitted_date, '%H:%i:%s') AS submitted_date_time FROM northwind.purchase_orders;
-SELECT HOUR(submitted_date) AS submitted_hour FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_hour FROM purchase_orders;
